21993c5b91baec4e2feb6208aa03b2432562b405
Ann-Anyango <annmwangombe@gmail.com>
2023-02-02 22:04:15 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var name ="Ann-Anyango"
+    println(name)
+    var age :Byte = 20
+    println( age)
+    var phonenumber : String = "0701852120"
+    println(phonenumber)
+    val  weight =64
+    var citizenshipkenya:Boolean =true
+    println(citizenshipkenya)
+
+
+
+
+
+
+
+}
